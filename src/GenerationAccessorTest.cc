4169eeda1510cfc4577fcf05fd48679aa4e1019a
adammartin <ebondark.od@gmail.com>
2011-09-08 00:07:47 -0400

    adding some hardening tests that happen to pass but still speak to requirements

--- src/GenerationAccessorTest.cc
+++ src/GenerationAccessorTest.cc
@@ -52,6 +52,32 @@ TEST(GenerationAccessor, CanValidateBoardCallCorrectRulesForAllAlive){
 	EXPECT_EQ(expectedGrid, accessor.access(firstGen));
 }
 
+TEST(GenerationAccessor, WillSetBoardBasedOnRules){
+	const int data_size=3;
+
+	Grid firstGen(boost::extents[data_size][data_size]);
+	Grid expectedGrid(boost::extents[data_size][data_size]);
+
+	MockRules livingRules;
+	MockRules deadRules;
+
+	for(short x = 0; x < data_size; x++){
+		for(short y = 0; y < data_size; y++){
+			firstGen[x][y] = true;
+		}
+	}
+
+	expectedGrid[1][1] = true;
+
+	EXPECT_CALL(livingRules, Apply(3)).Times(Exactly(4)).WillRepeatedly(Return(false));
+	EXPECT_CALL(livingRules, Apply(5)).Times(Exactly(4)).WillRepeatedly(Return(false));
+	EXPECT_CALL(livingRules, Apply(8)).Times(Exactly(1)).WillRepeatedly(Return(true));
+
+	GenerationAccessor accessor(livingRules, deadRules);
+
+	EXPECT_EQ(expectedGrid, accessor.access(firstGen));
+}
+
 TEST(GenerationAccessor, CanValidateBoardCallCorrectRulesForMixed){
 	const int data_size=3;
 
@@ -65,6 +91,32 @@ TEST(GenerationAccessor, CanValidateBoardCallCorrectRulesForMixed){
 	firstGen[0][1] = true;
 
 	EXPECT_CALL(deadRules, Apply(0)).Times(Exactly(3)).WillRepeatedly(Return(false));
+	EXPECT_CALL(deadRules, Apply(1)).Times(Exactly(2)).WillRepeatedly(Return(false));
+	EXPECT_CALL(deadRules, Apply(2)).Times(Exactly(2)).WillRepeatedly(Return(false));
+	EXPECT_CALL(livingRules, Apply(1)).Times(Exactly(2)).WillRepeatedly(Return(false));
+
+	GenerationAccessor accessor(livingRules, deadRules);
+
+	EXPECT_EQ(expectedGrid, accessor.access(firstGen));
+}
+
+TEST(GenerationAccessor, WillApplyCorrectRulesForMixed){
+	const int data_size=3;
+
+	Grid firstGen(boost::extents[data_size][data_size]);
+	Grid expectedGrid(boost::extents[data_size][data_size]);
+
+	MockRules livingRules;
+	MockRules deadRules;
+
+	firstGen[0][0] = true;
+	firstGen[0][1] = true;
+
+	expectedGrid[2][0] = true;
+	expectedGrid[2][1] = true;
+	expectedGrid[2][2] = true;
+
+	EXPECT_CALL(deadRules, Apply(0)).Times(Exactly(3)).WillRepeatedly(Return(true));
 	EXPECT_CALL(deadRules, Apply(1)).Times(Exactly(2)).WillRepeatedly(Return(false));
 	EXPECT_CALL(deadRules, Apply(2)).Times(Exactly(2)).WillRepeatedly(Return(false));
 	EXPECT_CALL(livingRules, Apply(1)).Times(Exactly(2)).WillRepeatedly(Return(false));
