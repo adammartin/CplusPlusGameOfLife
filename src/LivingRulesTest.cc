9fceda64697dc581a4907d9245e1fad5f88e4dc2
adammartin <ebondark.od@gmail.com>
2011-09-17 21:31:45 -0500

    minor refactor

--- src/LivingRulesTest.cc
+++ src/LivingRulesTest.cc
@@ -1,16 +1,15 @@
-#include "Rules.h"
 #include "LivingRules.h"
 #include <gtest/gtest.h>
 
 TEST(Rules, ApplyToLivingCells){
-	Rules* rules = new LivingRules;
-	EXPECT_EQ(false, rules->Apply(0));
-	EXPECT_EQ(false, rules->Apply(1));
-	EXPECT_EQ(true, rules->Apply(2));
-	EXPECT_EQ(true, rules->Apply(3));
-	EXPECT_EQ(false, rules->Apply(4));
-	EXPECT_EQ(false, rules->Apply(5));
-	EXPECT_EQ(false, rules->Apply(6));
+	LivingRules rules;
+	EXPECT_EQ(false, rules.Apply(0));
+	EXPECT_EQ(false, rules.Apply(1));
+	EXPECT_EQ(true, rules.Apply(2));
+	EXPECT_EQ(true, rules.Apply(3));
+	EXPECT_EQ(false, rules.Apply(4));
+	EXPECT_EQ(false, rules.Apply(5));
+	EXPECT_EQ(false, rules.Apply(6));
 }
 
 
